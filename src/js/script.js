72c54a26bfe0a8f5ff0234eea4cc935cd28da3a2
Des <Desmondmls@gmail.com>
2023-12-14 23:03:53 +0200

    hw_3 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const operand1 = Number(prompt('Введіть перше число'));
+const operand2 = Number(prompt('Введіть друге число'));
+
+// +
+let sumResult = operand1 + operand2;
+console.log(`Результат: ${operand1} + ${operand2} = ${sumResult}`);
+
+// -
+let subtractionResult = operand1 - operand2;
+console.log(`Результат: ${operand1} - ${operand2} = ${operand1 - operand2}`);
+// *
+let multiplicationResult = operand1 * operand2;
+console.log(`Результат: ${operand1} * ${operand2} = ${operand1 * operand2}`);
+// Ділення
+let divisionResult = operand1 / operand2;
+console.log(`Результат: ${operand1} / ${operand2} = ${operand1 / operand2}`);
