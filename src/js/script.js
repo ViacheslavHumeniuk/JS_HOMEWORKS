88bbdd50f8afc34f8bfa35287ea8c90401a85912
Des <Desmondmls@gmail.com>
2023-12-14 18:20:53 +0200

    HW_2 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//Task #1
+let admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+
+//Task #2
+let planetName ='Земля';
+let visitorName ="Ваше Ім'я";
+
+//Task #3
+let myVariable = '10' + 10;
+console.log(typeof myVariable);
