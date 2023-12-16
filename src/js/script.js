71feae3e5a9b6dfa261332e592b0d0a282c88d6b
Des <Desmondmls@gmail.com>
2023-12-16 02:06:54 +0200

    hw_2 Completed add changes

--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,15 @@ const operand1 = Number(prompt('Введіть перше число'));
 const operand2 = Number(prompt('Введіть друге число'));
 
 // +
-let sumResult = operand1 + operand2;
+const sumResult = operand1 + operand2;
 console.log(`Результат: ${operand1} + ${operand2} = ${sumResult}`);
 
 // -
-let subtractionResult = operand1 - operand2;
+const subtractionResult = operand1 - operand2;
 console.log(`Результат: ${operand1} - ${operand2} = ${operand1 - operand2}`);
 // *
-let multiplicationResult = operand1 * operand2;
+const multiplicationResult = operand1 * operand2;
 console.log(`Результат: ${operand1} * ${operand2} = ${operand1 * operand2}`);
 // Ділення
-let divisionResult = operand1 / operand2;
+const divisionResult = operand1 / operand2;
 console.log(`Результат: ${operand1} / ${operand2} = ${operand1 / operand2}`);
