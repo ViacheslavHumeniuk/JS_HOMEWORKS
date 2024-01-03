65e4f9d76ca0ed7571cf730a81169169c39ebb9c
Des <Desmondmls@gmail.com>
2024-01-04 00:37:28 +0200

    hw_12 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const firstNum = +prompt('ВЕДІТЬ ПЕРШЕ ЧИСЛО');
+const secondNum = +prompt('ВЕДІТЬ ДРУГЕ ЧИСЛО');
+
+const sumPlus = firstNum + secondNum ;
+alert(firstNum + secondNum);
+
+const sumMinus = firstNum - secondNum;
+alert(firstNum - secondNum);
+
+const sumMulti = firstNum * secondNum;
+alert(firstNum * secondNum );
+
+const sumDiv = firstNum / secondNum;
+alert(secondNum / firstNum)
