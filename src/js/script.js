52bde6d2ddf815d0fc2e75613a0b45cac5ecb4eb
Des <Desmondmls@gmail.com>
2024-01-04 17:46:51 +0200

    hw_15 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const num1 = prompt("введіть перше число")
+const num2 = prompt("введіть друге число")
+const num3 = prompt("введіть третє число")
+
+const average = (num1 + num2 + num3) / 3;
+
+alert("Середнэ арифметичне чисел дорівнює " + average);
