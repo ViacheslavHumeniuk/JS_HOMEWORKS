d3c94a0ef4ec1a02c0a3b64afd41a5116568a342
Des <Desmondmls@gmail.com>
2024-01-04 14:18:23 +0200

    hw_13 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const calculate = prompt('виберіть дію (add, sub, mult, div)')
+const num1 = +prompt("Перше число")
+const num2 = +prompt("Друге число")
+
+if (calculate === "+") {
+    alert(num1 + '+' + num2 + "=" + (num1 + num2));
+} else if (calculate === "-") {
+    alert(num1 + '-' + num2 + "=" + (num1 - num2));
+} else if (calculate === "*") {
+    alert(num1 + '*' + num2 + "=" + (num1 * num2));
+} else if (calculate === "/") {
+    alert(num1 + '/' + num2 + "=" + (num1 / num2));
+}
