43acce60f2302706cba36e2751a412505aed0ff0
Des <Desmondmls@gmail.com>
2024-01-10 01:44:08 +0200

    hw_24 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const user = prompt("Введіть число");
+let num = parseInt(user);
+if (!isNaN(num) && num >= 1) {
+    let min = 0;
+    for (let divider = 2; divider <= num; divider++) {
+        if (num % divider === 0) {
+            min = divider;
+            break;
+        }
+    }
+    if (min !== 0) {
+        console.log(`Мінімальний дільник числа ${num}: ${min}`)
+    }
+} else {
+    console.log("NaN");
+
+}
