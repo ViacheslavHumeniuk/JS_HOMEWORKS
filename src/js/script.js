61391a237faef2cf950d69829164fa235adc09e0
Des <Desmondmls@gmail.com>
2024-01-10 00:44:14 +0200

    hw_25 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-'use strict';
+'use strict'
+
+let a = 0;
+
+startLoop: for (let b = 1; b <= 20; b++) {
+    if (b % 2 === 0) {
+        continue startLoop;
+    }
+    a += b;
+}
+alert(`Сума непарних чисел: ${a}`);
