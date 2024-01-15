b146b9a6e369a7190ea13bff07a4f16fc47e73a1
Des <Desmondmls@gmail.com>
2024-01-16 01:16:35 +0200

    hw_26 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const characters = 'qwertyuiop1234567890';
+const length = 16;
+
+function generateKey(length, characters) {
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        const random = Math.floor(Math.random() * characters.length);
+        key += characters.charAt(random);
+    }
+    return key;
+}
+
+console.log(generateKey(length, characters));
