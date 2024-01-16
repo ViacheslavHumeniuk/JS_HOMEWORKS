a27008c5cd133173f0c899163c5f857401076738
Des <Desmondmls@gmail.com>
2024-01-16 21:16:53 +0200

    hw_30 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const potatoWeight = 75;
+const oneKilogramsPotatoPrice = 13;
+const litersBorsch = 48;
+const oneLiterPotato = 4;
+
+function calculateSum(potatoWeight, litersBorsch, oneKilogramsPotatoPrice, oneLiterPotato) {
+    const usingPotatoPerLiter = litersBorsch * oneLiterPotato; // 192
+    const potatoKilograms = usingPotatoPerLiter * potatoWeight; // 14.400
+    const potatoPrice = (potatoKilograms / 1000) * oneKilogramsPotatoPrice;
+    return Math.floor(potatoPrice);
+}
+
+console.log(calculateSum(potatoWeight, litersBorsch, oneKilogramsPotatoPrice, oneLiterPotato));
