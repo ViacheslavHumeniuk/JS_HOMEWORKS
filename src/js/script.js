2a1635c4a39c5fc4c278ff800019e06c61293908
Des <Desmondmls@gmail.com>
2024-01-16 20:02:50 +0200

    hw_29 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function addArray(arr) {
+    if (arr.length === 0) {
+        throw new Error('Масив є порожній')
+    }
+    const result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) result.push(arr[i]);
+    }
+    return result.length === 0 ? null : result;
+}
+
+console.log(addArray(arr));
