c08b90fd00be161935844524b8484e4a5abc9f7d
Des <Desmondmls@gmail.com>
2024-01-19 21:59:32 +0200

    hw_31 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-'use strict';
+'use strict'
+
+const getNumber = [];
+
+function getRandomNumber (min, max)
+{
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while
+        (getNumber.includes (randomNumber));
+    getNumber.push(randomNumber);
+    return randomNumber;
+}
+
+for (let i = 0; i <100; i++) {
+    let newNumber = getRandomNumber (1,100);
+    console.log (newNumber);
+}
