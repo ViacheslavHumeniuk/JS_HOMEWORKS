248d568895f1da95c064a0231b66b23fb097a9f8
Des <Desmondmls@gmail.com>
2024-01-20 22:14:40 +0200

    hw_31 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,8 @@ const number = +prompt('Введіть число');
 const riseStep = +prompt('Введіть ступень')
 
 function numberDegree(number, riseStep = 1) {
-    if (typeof number != 'number')
-        return 'Введіть коректні дані';
-    if (typeof riseStep != 'number')
-        return 'Введіть коректні дані';
-
+    if (typeof number != 'number') return 'Введіть коректні дані';
+    if (typeof riseStep != 'number') return 'Введіть коректні дані';
     return number ** riseStep;
 }
 
