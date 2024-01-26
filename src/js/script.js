cb6e82371249b9aaa50c7d51eb39cb85fc715ac6
Des <Desmondmls@gmail.com>
2024-01-26 22:06:55 +0200

    hw_34 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-'use strict';
+'use strict'
+
+const createUser = function (name, age) {
+  const user = { name: [name], age: [age] }
+  user.greet = greet
+  return user
+}
+const greet = function () {
+  console.log(`Hello, my name is ${this.name}`)
+}
+
+const user1 = createUser('Mike', 23); user1.greet()
+const user2 = createUser('Emily', 18); user2.greet()
