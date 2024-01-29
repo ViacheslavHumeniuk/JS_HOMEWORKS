e4fd3cad98d70effe023cf3ea9660942801544dc
Des <Desmondmls@gmail.com>
2024-01-29 20:44:00 +0200

    hw_37 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-'use strict';
+'use strict'
+
+const parseUrl = function (url) {
+  const urlObject = new URL(window.location.href)
+
+  return {
+    protocol: urlObject.protocol,
+    host: urlObject.host,
+    pathname: urlObject.pathname,
+    search: urlObject.search,
+    origin: urlObject.origin
+  }
+}
+const url = 'https://example.com:8080/path/name?query=123#hash'
+const urlObject = parseUrl(url)
+console.log(parseUrl(url))
