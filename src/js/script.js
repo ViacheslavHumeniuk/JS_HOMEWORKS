f4e7eec4a62ba7d5768c11fa8600cbc630e20f03
Des <Desmondmls@gmail.com>
2024-02-02 20:44:22 +0200

    hw_20 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const year = +prompt('Введіть число')
+if (isNaN(year)) throw new Error('Ви ввели не число')
+switch (year) {
+  case 1:
+    alert(`${year} рік`)
+    break
+  case 2:
+  case 3:
+  case 4:
+    alert(`${year} роки`)
+    break
+  default:
+    alert(`${year} років`)
+}
