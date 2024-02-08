ac7ab0d773deebacdb24acc44db3e807e5812b76
Des <Desmondmls@gmail.com>
2024-02-08 19:58:54 +0200

    hw_40 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const numbers = [5, 4, 12, 19, 12, 2, 6, 8, 17, 10]
+
+const getAverage = (numbers) => {
+  const total = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, 0)
+  return total / numbers.length
+}
+
+console.log(getAverage(numbers))
