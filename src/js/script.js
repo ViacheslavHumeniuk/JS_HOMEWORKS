83584283cd67ec05fb36efabe5d0eb77cebb187a
Des <Desmondmls@gmail.com>
2024-02-20 18:59:49 +0200

    hw_40 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,16 @@
 'use strict'
 
-const numbers = [5, 4, 12, 19, 12, 2, 6, 8, 17, 10]
+const customReduce = (array, callback, initialValue) => {
+  let accumulator = initialValue === undefined ? array[0] : initialValue
+  const startIndex = initialValue === undefined ? 1 : 0
 
-const getAverage = (numbers) => {
-  const total = numbers.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue
-  }, 0)
-  return total / numbers.length
+  for (let i = startIndex; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i], i, array)
+  }
+
+  return accumulator
 }
 
-console.log(getAverage(numbers))
+const numbers = [5, 4, 12, 19, 12, 2, 6, 8, 17, 10]
+const sum = customReduce(numbers, (accumulator, currentValue) => accumulator + currentValue, 0)
+console.log(sum)
