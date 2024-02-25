6613a9bc2387bc27c1da32f87b3be198b246e001
Des <Desmondmls@gmail.com>
2024-02-25 22:02:37 +0200

    hw_48 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+// 1
+const sum = (a, b) => a + b
+console.log(sum(2, 2))
+
+// 2
+
+const stringLength = str => {
+  if (typeof str === 'string') {
+    return str.length
+  }
+}
+console.log(stringLength('Hello'))
+
+// 3
+
+const add = (arr) => {
+  const result = []
+  for (const num of arr) {
+    result.push(num + 1)
+  }
+  return result
+}
+console.log(add([1, 2, 3, 4, 5]))
