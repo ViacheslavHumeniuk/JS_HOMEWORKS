3ce02cdcc101e8a58007778eb787da152f514c5c
Des <Desmondmls@gmail.com>
2024-03-21 18:11:42 +0200

    hw_47 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const Mybind = function (fn, context, ...boundArgs) {
+  return function (...args) {
+    return fn.apply(context, [...boundArgs, ...args])
+  }
+}
+const greet = function (greeting, punctuation) {
+  return greeting + ' ' + this.user + punctuation
+}
+const user = { user: 'Alex' }
+const greetAlex = Mybind(greet, user, 'Hello')
+console.log(greetAlex('!'))
