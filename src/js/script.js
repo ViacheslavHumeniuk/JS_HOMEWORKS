2bf95bf38e2ff5c715d07269076cfc194f42549d
Des <Desmondmls@gmail.com>
2024-03-21 18:28:47 +0200

    hw_51 Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const parseJSON = (jsonString) => {
+  try {
+    return JSON.parse(jsonString)
+  } catch (error) {
+    throw new Error('Invalid JSON string')
+  }
+}
+
+try {
+  console.log(parseJSON('{"name": "John", "age": 30}'))
+  console.log(parseJSON('{"name": "John", "age":'))
+} catch (error) {
+  console.log(error.message)
+}
