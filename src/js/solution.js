b785a09cd79a6f0b68f69f76eebd78a21f38bc25
Des <Desmondmls@gmail.com>
2024-04-04 15:49:24 +0300

    main

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,9 +1,1 @@
 
-const ulElement = document.getElementById('ulId');
-const liElements = ulElement.getElementsByTagName('li');
-const textArray = []
-for (const li of liElements) {    console.log(li.textContent);
-    console.log(liElements.length)    }
-for (const li of liElements) {
-    textArray.push(li.textContent)}
-console.log(textArray)
