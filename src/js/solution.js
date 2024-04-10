75aa264d2db0c4279295c2b17c308f052a43a82d
Des <Desmondmls@gmail.com>
2024-04-10 20:50:45 +0300

    hw_55 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,22 @@
 
+'use strict'
+
+const createTable = (rows, column) => {
+    const table = document.createElement('table')
+    for (let i = 0; i < rows; i++) {
+        const rowElement = document.createElement('tr')
+        for (let j = 0; j < column; j++) {
+            const cellElement = document.createElement('td')
+            rowElement.appendChild(cellElement)
+            if (i === 0) {
+                cellElement.textContent = j + 1
+            } else {
+                cellElement.textContent = i * 10 + j + 1
+            }
+        }
+        table.appendChild(rowElement)
+    }
+
+    document.body.appendChild(table)
+}
+createTable(10, 10)
