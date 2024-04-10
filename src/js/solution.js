98313014898da3eb7203388f982311998fe4039d
Des <Desmondmls@gmail.com>
2024-04-10 20:53:15 +0300

    hw_58 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,9 @@
-
+"use strict";
+const isEven = function () {
+  const numberInput = document.getElementById("numberInput").value;
+  if (numberInput % 2 === 0) {
+    alert("Число парне");
+  } else {
+    alert("Число непарне");
+  }
+};
