27f566f59d537a5b4d54a145d39cf90634c6a0f3
Des <Desmondmls@gmail.com>
2024-04-10 20:56:24 +0300

    hw_59 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,13 @@
+"use strict";
 
+const saveElem = document.getElementById("saveElem");
+
+const saveNotes = () => {
+  const inputTxt = document.getElementById("textInput").value;
+  if (inputTxt === "" || inputTxt.trim() === "") {
+    alert("Please enter a value");
+  }
+  const elem = document.createElement("p");
+  elem.innerHTML = inputTxt;
+  saveElem.prepend(elem);
+};
