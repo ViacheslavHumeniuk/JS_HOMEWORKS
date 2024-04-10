f5ea814b933afce1205babee7f125053b904040e
Des <Desmondmls@gmail.com>
2024-04-10 20:45:15 +0300

    hw_53 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,15 @@
+'use strict'
 
+const ul = document.getElementById('ulId')
+const attributes = []
+const nameAttributes = []
+
+for (const attr of ul.attributes) {
+    attributes.push(attr.value)
+    nameAttributes.push(attr.name)
+}
+console.log(attributes, nameAttributes)
+
+ul.lastElementChild.textContent = 'Привіт мене звати Славік'
+ul.firstElementChild.setAttribute('data-my-name', 'Славік')
+ul.removeAttribute('data-dog-tail')
