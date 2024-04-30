752b563fafdb6bdada5ccf78db5473a119b1820c
Des <Desmondmls@gmail.com>
2024-05-01 01:23:53 +0300

    hw_60 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,18 @@
+"use strict";
 
+document
+  .getElementById("registration")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    auditionFunction(data);
+  });
+const auditionFunction = function (data) {
+  if (!data.name || !data.email || !data.password) {
+    document.getElementsByTagName("p")[0].innerHTML =
+      "Please fill in all fields";
+  } else {
+    document.getElementsByTagName("p")[0].innerHTML = "Registration successful";
+  }
+};
