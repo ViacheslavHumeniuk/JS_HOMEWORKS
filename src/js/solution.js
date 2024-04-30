f9d2a146fac641fce51f948a26e797836ec5529d
Des <Desmondmls@gmail.com>
2024-05-01 01:31:10 +0300

    hw_60 Completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,39 +1,18 @@
 "use strict";
 
-const answers = document.getElementById("answers");
-const polls = document.getElementById("polls");
 document
-  .getElementsByTagName("form")[0]
+  .getElementById("registration")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    auditionFunction(data);
   });
-document.getElementById("button").addEventListener("click", function (event) {
-  event.preventDefault();
-  const answerVariant = document.createElement("input");
-  document.getElementById("answers").appendChild(answerVariant);
-});
-document.getElementById("poll").addEventListener("click", function (event) {
-  event.preventDefault();
-  if (
-    document.getElementsByTagName("input")[0].value === "" ||
-    answers.children.length < 2 ||
-    answers.children[0].value === "" ||
-    answers.children[1].value === ""
-  ) {
-    throw new Error("заповніть всі поля");
+const auditionFunction = function (data) {
+  if (!data.name || !data.email || !data.password) {
+    document.getElementsByTagName("p")[0].innerHTML =
+      "Please fill in all fields";
   } else {
-    const h2 = document.createElement("h2");
-    const ol = document.createElement("ol");
-
-    h2.textContent = document.getElementsByTagName("input")[0].value;
-
-    for (let i = 0; i < answers.children.length; i++) {
-      const li = document.createElement("li");
-      li.textContent = answers.children[i].value;
-      ol.appendChild(li);
-    }
-
-    polls.appendChild(h2);
-    polls.appendChild(ol);
+    document.getElementsByTagName("p")[0].innerHTML = "Registration successful";
   }
-});
+};
