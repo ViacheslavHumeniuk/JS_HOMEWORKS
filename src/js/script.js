3824f2e743c2c7511f3a1cc1a3cd66ebb4d8b81e
Des <Desmondmls@gmail.com>
2024-07-14 17:26:38 +0300

    hw_69 Complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
-'use strict'
+const propertyGenerator = async function * (url) {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  for (const obj of data) {
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        yield { [key]: obj[key] }
+      }
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const propertyList = document.getElementById('property-list')
+  const generator = propertyGenerator('https://jsonplaceholder.typicode.com/users')
+
+  const displayNextProperty = async function () {
+    const { value, done } = await generator.next()
+    if (done) {
+      clearInterval(intervalId)
+      return
+    }
+
+    const listItem = document.createElement('li')
+    listItem.className = 'list-group-item'
+    listItem.textContent = JSON.stringify(value)
+    propertyList.appendChild(listItem)
+  }
+
+  const intervalId = setInterval(displayNextProperty, 500)
+})
