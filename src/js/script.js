ec1947da97749e634ef94505103626732e55833d
Des <Desmondmls@gmail.com>
2024-07-14 17:19:52 +0300

    main update

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,1 @@
 'use strict'
-document.getElementById('form').addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  const formData = new FormData(this)
-  const formObject = {}
-
-  formData.forEach((value, key) => {
-    formObject[key] = value
-  })
-
-  localStorage.setItem('formData', JSON.stringify(formObject))
-  alert('Form data saved!')
-})
