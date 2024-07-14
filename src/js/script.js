20c92bfa827897d3adfa19644442acf316e8bed9
Des <Desmondmls@gmail.com>
2024-07-14 17:33:21 +0300

    hw_70 Complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+const createArrayIterator = function (arr) {
+  let index = 0
+
+  return {
+    [Symbol.iterator]: function () {
+      return this
+    },
+    next: function () {
+      if (index < arr.length) {
+        return { value: arr[index++], done: false }
+      } else {
+        return { value: undefined, done: true }
+      }
+    }
+  }
+}
